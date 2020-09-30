a01d5289d12f70fc344fae104abbbf9824d5975e
Nikhil9hac <59399754+Nikhil9hac@users.noreply.github.com>
2020-10-01 01:11:01 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -3,6 +3,7 @@ import PySimpleGUI as sg
 import wikipedia
 import pyttsx3
 import speech_recognition as sr
+import smtplib
 
 client = wolframalpha.Client('4UW6LT-A3WR96V43Q')
 engine = pyttsx3.init()
@@ -16,7 +17,14 @@ layout = [[sg.Text('Hello, I am JARVIS, please ask me a question')],
 
 # Create the Window
 window = sg.Window('JARVIS Supercomputer', layout)
-
+# send email function
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('youremail@gmail.com', 'your-password')
+    server.sendmail('youremail@gmail.com', to, content)
+    server.close()
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
@@ -60,7 +68,7 @@ while True:
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
     # Logic allows user to type out a question
-    if event == 'Type a question':
+            if event == 'Type a question':
         layout = [[sg.Text('Please type your question below')],
                   [sg.InputText()],
                   [sg.Button('Ok'), sg.Button('Cancel')]]
@@ -93,7 +101,17 @@ while True:
                 engine.say(wiki_res)
                 sg.PopupNonBlocking(wiki_res)
                 if event in (None, 'Cancel'):  # if user closes window or clicks cancel
-                    break
+                    break                                        
+            if event == 'Send an email':
+                    try:
+                        speak("What should I say?")
+                        content = takeCommand()
+                        to = "harryyourEmail@gmail.com"    
+                        sendEmail(to, content)
+                        speak("Email has been sent!")
+                    except Exception as e:
+                               print(e)
+                               speak("Sorry my friend harry bhai. I am not able to send this email")
 
     engine.runAndWait()
 
